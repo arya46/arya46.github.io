9d8c2158748a6d699e51475121cf75dc4ecd5e13
arya.46 <tulrosdeori@gmail.com>
2020-06-11 20:52:16 +0530

    Added files from local

---
+++ js/db_config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBysGsbPi4DorxCKKIr3Qn7wA6tS3K6dlE",
+    authDomain: "my-website-e5a37.firebaseapp.com",
+    databaseURL: "https://my-website-e5a37.firebaseio.com",
+    projectId: "my-website-e5a37",
+    storageBucket: "my-website-e5a37.appspot.com",
+    messagingSenderId: "383705476944",
+    appId: "1:383705476944:web:449439a4f535f264f9c655",
+    measurementId: "G-XTS7549LCB"
+  };
+
